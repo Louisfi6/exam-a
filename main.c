99ff392d995682b96e8afcc6b3d011c5639660c6
Louisfi6 <116919020+Louisfi6@users.noreply.github.com>
2023-03-03 12:57:37 +0100

    Louis Sudoku

--- main.c
+++ main.c
@@ -3,82 +3,137 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define GRID_SIZE  9
+#define LIST_SIZE  9
+#define LIST_SIZE  9
 
-/*
-Pour l'exercice vous aurez besoin de generer des entiers aléatoire : rand() renvoit un entier aléatoire.
-il s'utilise : rand() % NOMBREMAX + 1 
-Pour un entier aléatoire entre 0 et 1 il faut donc faire rand() %2
-voir dans la methode main.
-*/
+int liste[LIST_SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 
+//Fonction saisir() pour remplir la grille de sudoku en mettant i j et la valeur v tant que remplissage est inferieur a 81
 
+int saisir(int grid[GRID_SIZE][GRID_SIZE]){
+    //faire un boucle while pour remplir la grille de sudoku
 
+    int i, j, v;
+    int remplissage = 0;
 
-
-// Ecrire la fonction generer(), elle prend en paramètre la grille et renvoie le nombre d'éléments non nuls
-
-// Ce lien vous sera utile : https://www.geeksforgeeks.org/pass-2d-array-parameter-c/
-
-
-/*Écrire une fonction saisir() qui demande à l’utilisateur de saisir au clavier les indices i et j et la valeur v à placer à l’emplacement (i,j).
-La fonction doit vérifier la validité des indices et de la valeur.
-Si la case n’est pas occupée, la valeur doit être placée dans la grille. remplissage est alors incrémentée*/
-
-
-/*
-Écrire la fonction verifierLigneColonne() qui prend en paramètre un numéro et un sens (HORIZ ou VERT)
-qui vérifie que la ligne ou la colonne (suivant les cas) numéro est bien remplie.
-On pourra utiliser un tableau intermédiaire pour vérifier cela. La fonction retournera 1 si tout s’est bien passé, 0 sinon.
- Les constantes HORIZ de valeur 0 et VERT de valeur 1 sont à définir.
-*/
-
-/*
-Écrire la fonction verifierRegion() qui prend en paramètre deux indices k et l qui correspondent à la région (k,l)
-et qui renvoie 1 si la région est correctement remplie, 0 sinon.
-*/
-
-//Écrire la fonction verifierGrille() qui renvoie 1 si la grille est correctement remplie et 0 sinon
-
-
-//Écrire le programme principal, en supposant que la seule condition d’arrêt est la réussite du sudoku (ce test ne devra être fait que si nécessaire)
-
-
-int main(){
-    // Ne pas toucher le code entre les commentaires
-    srand( time( NULL ) );
-
-
-    int i, j, k;
-    int solution[9][9];
-    printf("SOLUTION");  
-    printf("\n");  
-    printf("---------------------------------");  
-    printf("\n");  
-    for(j=0;j<9; ++j) 
-    {
-    for(i=0; i<9; ++i)
-        solution[j][i] = (i + j*3 +j /3) %9 +1 ; 
+    while(remplissage <= 81){
+        printf("Saisissez les indices i et j et la valeur v a placer a l emplacement (i,j) dans la grille de sudoku : \n");
+        scanf("\n%d\n %d\n %d", &i, &j, &v);
+        if (i < 0 || i > 8 || j < 0 || j > 8 || v < 1 || v > 9){
+            printf("Erreur de saisie");
+            return 0;
+        }
+        if (grid[i][j] != 0){
+            printf("La case deja occupee");
+            return 0;
+        }
+        grid[i][j] = v;
+        printf("Voici le sudoku que vous avez rempli : \n");
+        for (i = 0; i < GRID_SIZE; i++) { // boucle for qui va afficher la grid
+            if(i % 3 == 0)
+                printf("+-------+-------+-------+\n");
+            for (j = 0; j < GRID_SIZE; j++) {
+                if(j % 3 == 0)
+                    printf("| ");
+                printf("%d ", grid[i][j]);
+            }
+            printf("| \n");
+        }printf("+-------+-------+-------+\n");
+        remplissage++;
     }
-    
-    for(i=0;i<9; ++i) 
-    {
-    for(j=0; j<9; ++j)
-        printf("%d ", solution[i][j]);
-    printf("\n");  
-    }
-    printf("---------------------------------");  
-    printf("\n");  
-    //toucher le code entre les commentaires 
-    
-    printf("%d",rand() % 2);
-    
-  
-
-
-
-    //Ne pas toucher au code ci dessous
-    system("pause");
-    return 0;
 }
 
+
+//Fonction qui va vérifier si la grid renvoie par la fonction saisir() est la meme que solvedGrid
+
+bool verifier(int grid[GRID_SIZE][GRID_SIZE], int solvedGrid[GRID_SIZE][GRID_SIZE]){
+    int i, j;
+    for (i = 0; i < GRID_SIZE; i++) {
+        for (j = 0; j < GRID_SIZE; j++) {
+            if (grid[i][j] != solvedGrid[i][j]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void melangerListe(){
+    int i, j, end;
+    for (i = 0; i < LIST_SIZE - 1; i++) { //parcourir la liste des nombres à mélanger
+        j = rand() % (LIST_SIZE - i) + i;
+        //algo de fisher yates pour mélanger les nombres
+        end = liste[i];
+        liste[i] = liste[j];
+        liste[j] = end;
+    }
+}
+
+int main(){
+    srand( time( NULL ) );
+    melangerListe();
+
+
+    int grid[GRID_SIZE][GRID_SIZE] = {0}; //création du tableau en 2D
+    int i, j, k; //variables pour les itérations
+    for (i = 0; i < GRID_SIZE; i++) {
+        for (j = 0; j < GRID_SIZE; j++) {
+            k = (i * 3 + i / 3 + j) % LIST_SIZE; // Calcul de la position du prochain numéro à placer
+            grid[i][j] = liste[k]; // Placement du numéro dans la grille
+        }
+    }
+
+    int num_to_remove  = 1; //30 nombres doivent être supprimés
+
+    while (num_to_remove > 0) {
+        int row = rand() % 9; //choisir une ligne au hasard
+        int col = rand() % 9; //choisir une colone au hasard
+        if (grid[row][col] != 0) { //si la grille avec la colone + ligne choisies est différent de 0
+            grid[row][col] = 0; // on met la case à 0
+            num_to_remove--; //décrémentation pour arriver enfin à 30 nombres supprimés
+        }
+    }
+
+    int solvedGrid[GRID_SIZE][GRID_SIZE]; //on copie la grid dans solvedGrid
+
+    for (i = 0; i < GRID_SIZE; i++) {
+        for (j = 0; j < GRID_SIZE; j++) {
+            solvedGrid[i][j] = grid[i][j]; //on parcourt la grid puis on copie les éléments dans solvedGrid. Au final on
+            //va venir compléter les cases = 0 par les chiffres manquants
+        }
+    }
+
+
+    printf("Voici le sudoku a resoudre : \n");
+    for (i = 0; i < GRID_SIZE; i++) { // boucle for qui va afficher la grid
+        if(i % 3 == 0)
+            printf("+-------+-------+-------+\n");
+        for (j = 0; j < GRID_SIZE; j++) {
+            if(j % 3 == 0)
+                printf("| ");
+            printf("%d ", grid[i][j]);
+        }
+        printf("| \n");
+    }
+    printf("+-------+-------+-------+\n");
+
+    //Demander à l'utilisateur de remplir la grid avec la fonction saisir()
+
+
+    saisir(grid);
+
+    if(saisir(grid) == verifier(grid, solvedGrid)){
+        printf("Bravo vous avez reussi a resoudre le sudoku");
+    }
+    else{
+        printf("Dommage vous n'avez pas reussi a resoudre le sudoku");
+    }
+
+
+
+
+//    //system("pause");
+    return 0;
+}
